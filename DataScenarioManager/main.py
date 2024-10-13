d576cf6d898fa7a46ff87f5c1140dbdd97f1bc19
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-10-13 21:02:40 +0900

    Commit

--- DataScenarioManager/main.py
+++ DataScenarioManager/main.py
@@ -63,45 +63,15 @@ def get_running_scenarios():
 # 시나리오 시작
 @app.post("/scenarios/{scenario_name}/start")
 def start_scenario(scenario_name: str):
-    scenario_path = os.path.join(SCENARIO_BASE_PATH, scenario_name)
-    if not os.path.exists(scenario_path):
-        raise HTTPException(status_code=404, detail="Scenario not found")
-
-    # 가상 환경과 start 스크립트 경로
-    venv_path = os.path.join(scenario_path, "venv", "bin", "activate")
-    start_script_path = os.path.join(scenario_path, "start.sh")
-
-    # 시나리오 실행 (예시로 start.sh 스크립트를 실행)
-    command = f"source {venv_path} && bash {start_script_path}"
-    process = subprocess.Popen(command, shell=True, cwd=scenario_path)
-
-    # 실행된 프로세스 ID 기록 (단순한 PID 파일 생성 방식 사용)
-    pid_file = os.path.join(scenario_path, "pid")
-    with open(pid_file, "w") as f:
-        f.write(str(process.pid))
-
-    return {"message": "Scenario started", "pid": process.pid}
+    data_scenario_manager_instance.run_scenario(scenario_name)
+    return {"message": "Scenario started", "uid": "1234"}
 
 
 # 시나리오 정지
 @app.post("/scenarios/{scenario_name}/stop")
-def stop_scenario(scenario_name: str):
-    scenario_path = os.path.join(SCENARIO_BASE_PATH, scenario_name)
-    pid_file = os.path.join(scenario_path, "pid")
-
-    if not os.path.exists(pid_file):
-        raise HTTPException(status_code=404, detail="Scenario is not running")
-
-    # PID를 읽어서 프로세스 종료
-    with open(pid_file, "r") as f:
-        pid = int(f.read())
-
-    try:
-        os.kill(pid, 9)  # 강제 종료
-        os.remove(pid_file)  # PID 파일 삭제
-        return {"message": "Scenario stopped"}
-    except ProcessLookupError:
-        raise HTTPException(status_code=404, detail="Process not found")
+def stop_scenario(scenario_uid: str):
+    data_scenario_manager_instance.stop_scenario(scenario_uid)
+    return {"message": "Scenario stopped", "uid": "1234"}
 
 
 if __name__ == '__main__':
