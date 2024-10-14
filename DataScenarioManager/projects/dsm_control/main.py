a65a6bc75ba21b5fc4f214463b7941f76f55c872
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-10-14 21:12:43 +0900

    Commit

--- DataScenarioManager/projects/dsm_control/main.py
+++ DataScenarioManager/projects/dsm_control/main.py
@@ -1,4 +1,35 @@
-sample = "Hello World"
-with open("Hello World.txt", "w") as f:
-    f.write(str(sample))
+import fakeredis
+import asyncio
 
+redis = fakeredis.FakeStrictRedis()
+redis.set("temperature", 0)
+
+# sample furniture
+async def stove():
+    while True:
+        temperature = int(redis.get("temperature"))
+        redis.set("temperature", temperature + 1)
+        await asyncio.sleep(1)
+
+# control function
+async def cooling_stove():
+    while True:
+        temperature = int(redis.get("temperature"))
+        if temperature > 10:
+            redis.set("temperature", temperature - 1)
+        await asyncio.sleep(1)
+
+'''
+async def stove_temperature():
+    while True:
+        temperature = int(redis.get("temperature"))
+        print(temperature)
+        await asyncio.sleep(1)
+'''
+
+async def main():
+    await asyncio.gather(stove(), cooling_stove())
+    #await asyncio.gather(stove(), cooling_stove(), stove_temperature())
+
+if __name__ == '__main__':
+    asyncio.run(main())
