e1dc80fd89d8c94cd50dca3eeb8c4464b05400dc
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-10-19 16:42:11 +0900

    consturct

--- data-scenario-center-backend/practice/conda2.py
+++ data-scenario-center-backend/practice/conda2.py
@@ -1,6 +1,8 @@
 import subprocess
 
 def run_in_conda_env(env_name, command):
-    full_command = f"conda run -n {env_name} {command}"
+    full_command = f"conda run -n {env_name} python {command}"
     result = subprocess.run(full_command, shell=True, capture_output=True, text=True)
     return result.stdout, result.stderr
+
+print(run_in_conda_env("base", "main.py"))
