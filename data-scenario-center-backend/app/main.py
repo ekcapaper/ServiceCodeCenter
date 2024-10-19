78a2404a05a359dedbd58b83b64ec05130f67063
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2024-10-19 12:46:00 +0900

    consturct

--- data-scenario-center-backend/app/main.py
+++ data-scenario-center-backend/app/main.py
@@ -6,8 +6,10 @@ from typing import List
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 from app.core.DataScenarioManager import data_scenario_manager_instance
+from app.api.main import api_router
 import logging
 
 # logging level
@@ -25,6 +27,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+# Set all CORS enabled origins
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
